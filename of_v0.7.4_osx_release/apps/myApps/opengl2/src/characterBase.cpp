d5d01c59f7ef707890e94d42f3c9ee30ed8629e2
Harrison Mendonça <h@harrison.com.br>
2013-04-03 00:11:46 -0300

    first commit

---
+++ of_v0.7.4_osx_release/apps/myApps/opengl2/src/characterBase.cpp
@@ -0,0 +1,57 @@
+#include "characterBase.h"
+
+characterBase::characterBase()
+{
+    //positions
+    
+    head.set(0, -80);
+    
+    shoulder_left.set(-10, -65);
+    elbow_left.set(-20, -50);
+    hand_left.set(-20, -30);
+    leg_left.set(-5, -40);
+    knee_left.set(-10, -20);
+    foot_left.set(-10, 0);
+    
+    shoulder_right.set(10, -65);
+    elbow_right.set(20, -50);
+    hand_right.set(20, -30);
+    leg_right.set(5, -40);
+    knee_right.set(10, -20);
+    foot_right.set(10, 0);
+    
+    
+    //material
+    
+    material.setEmissiveColor(ofColor(255,0,0));
+    material.setSpecularColor(ofColor(255,0,0));
+    material.setShininess(60);
+}
+
+void characterBase::draw()
+{
+    ofPushMatrix();
+    ofTranslate(position);
+    
+    material.begin();
+    
+    ofSphere(head, 10);
+    
+    ofSphere(shoulder_left, 5);
+    ofSphere(elbow_left, 5);
+    ofSphere(hand_left, 5);
+    ofSphere(leg_left, 5);
+    ofSphere(knee_left, 5);
+    ofSphere(foot_left, 5);
+    
+    ofSphere(shoulder_right, 5);
+    ofSphere(elbow_right, 5);
+    ofSphere(hand_right, 5);
+    ofSphere(leg_right, 5);
+    ofSphere(knee_right, 5);
+    ofSphere(foot_right, 5);
+    
+    material.end();
+    
+    ofPopMatrix();
+}
---
+++ of_v0.7.4_osx_release/libs/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofQTKitMovieRenderer.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Harrison/Dropbox/[Labs]\ OpenGL/opengl/of_v0.7.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/video/ofQTKitMovieRenderer.m \
+  ../../../../libs/openFrameworks/video/ofQTKitMovieRenderer.h
